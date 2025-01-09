0d4c8e47b17df439fa1ff654ca1bb168e221d03d
Mn1am1 <podbull@wp.pl>
2025-01-09 09:12:58 +0100

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', function () {
-    const startDate = new Date("2025-01-08T08:59:00");
+    const startDate = new Date("2025-01-09T08:59:00");
     const interval = (1 * 60 * 60 * 1000) + (2 * 60 * 1000) + (30 * 1000);
 
     let nextDate = calculateNextDate();
