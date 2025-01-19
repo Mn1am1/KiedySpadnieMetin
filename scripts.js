2de03e1801f9d5faaeb66d6d5e3a664e36130a73
Mn1am1 <podbull@wp.pl>
2025-01-19 02:04:22 +0100

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', function () {
-    const startDate = new Date("2025-01-16T11:07:00");
+    const startDate = new Date("2025-01-16T11:06:30");
     const interval = (1 * 60 * 60 * 1000) + (2 * 60 * 1000) + (30 * 1000);
 
     let nextDate = calculateNextDate();
